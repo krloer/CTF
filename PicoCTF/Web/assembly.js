4b7680eda1430c3643effdb803ec212d148ee704
krloer <kriloneri@gmail.com>
2022-09-27 22:28:08 +0200

    small changes to some assembly required and easy peasy

--- PicoCTF/Web/assembly.js
+++ PicoCTF/Web/assembly.js
@@ -1,47 +1,35 @@
-'use strict';
-const _0x402c = ["value", "2wfTpTR", "instantiate", "275341bEPcme", "innerHTML", "1195047NznhZg", "1qfevql", "input", "1699808QuoWhA", "Correct!", "check_flag", "Incorrect!", "./JIFxzHyW8W", "23SMpAuA", "802698XOMSrr", "charCodeAt", "474547vVoGDO", "getElementById", "instance", "copy_char", "43591XxcWUl", "504454llVtzW", "arrayBuffer", "2NIQmVj", "result"];
-const _0x4e0e = function(url, whensCollection) {
-  url = url - 470;
-  let _0x402c6f = _0x402c[url];
-  return _0x402c6f;
+const _0x402c = ['value', '2wfTpTR', 'instantiate', '275341bEPcme', 'innerHTML', '1195047NznhZg', '1qfevql', 'input', '1699808QuoWhA', 'Correct!', 'check_flag', 'Incorrect!', './JIFxzHyW8W', '23SMpAuA', '802698XOMSrr', 'charCodeAt', '474547vVoGDO', 'getElementById', 'instance', 'copy_char', '43591XxcWUl', '504454llVtzW', 'arrayBuffer', '2NIQmVj', 'result'];
+const _0x4e0e = function(_0x553839, _0x53c021) {
+    _0x553839 = _0x553839 - 0x1d6; // - 470
+    let _0x402c6f = _0x402c[_0x553839];
+    return _0x402c6f;
 };
-
-(function(data, oldPassword) {
-  const toMonths = _0x4e0e;
-  for (; !![];) {
-    try {
-      const userPsd = -parseInt(toMonths(491)) + parseInt(toMonths(493)) + -parseInt(toMonths(475)) * -parseInt(toMonths(473)) + -parseInt(toMonths(482)) * -parseInt(toMonths(483)) + -parseInt(toMonths(478)) * parseInt(toMonths(480)) + parseInt(toMonths(472)) * parseInt(toMonths(490)) + -parseInt(toMonths(485));
-      if (userPsd === oldPassword) {
-        break;
-      } else {
-        data["push"](data["shift"]());
-      }
-    } catch (_0x41d31a) {
-      data["push"](data["shift"]());
+(function(_0x76dd13, _0x3dfcae) {
+    const _0x371ac6 = _0x4e0e;
+    while (!![]) {
+        try {
+            const _0x478583 = -parseInt(_0x371ac6(0x1eb)) + parseInt(_0x371ac6(0x1ed)) + -parseInt(_0x371ac6(0x1db)) * -parseInt(_0x371ac6(0x1d9)) + -parseInt(_0x371ac6(0x1e2)) * -parseInt(_0x371ac6(0x1e3)) + -parseInt(_0x371ac6(0x1de)) * parseInt(_0x371ac6(0x1e0)) + parseInt(_0x371ac6(0x1d8)) * parseInt(_0x371ac6(0x1ea)) + -parseInt(_0x371ac6(0x1e5));
+            if (_0x478583 === _0x3dfcae) break;
+            else _0x76dd13['push'](_0x76dd13['shift']());
+        } catch (_0x41d31a) {
+            _0x76dd13['push'](_0x76dd13['shift']());
+        }
     }
-  }
-})(_0x402c, 627907);
-
+}(_0x402c, 0x994c3));
 let exports;
-(async() => {
-  const findMiddlePosition = _0x4e0e;
-  let leftBranch = await fetch(findMiddlePosition(489));
-  let rightBranch = await WebAssembly[findMiddlePosition(479)](await leftBranch[findMiddlePosition(474)]());
-  let module = rightBranch[findMiddlePosition(470)];
-  exports = module["exports"];
+(async () => {
+    const _0x48c3be = _0x4e0e;
+    let _0x5f0229 = await fetch(_0x48c3be(0x1e9)),
+        _0x1d99e9 = await WebAssembly[_0x48c3be(0x1df)](await _0x5f0229[_0x48c3be(0x1da)]()),
+        _0x1f8628 = _0x1d99e9[_0x48c3be(0x1d6)];
+    exports = _0x1f8628['exports'];
 })();
 
 function onButtonPress() {
-  const navigatePop = _0x4e0e;
-  let params = document["getElementById"](navigatePop(484))[navigatePop(477)];
-  for (let i = 0; i < params["length"]; i++) {
-    exports[navigatePop(471)](params[navigatePop(492)](i), i);
-  }
-  exports["copy_char"](0, params["length"]);
-  if (exports[navigatePop(487)]() == 1) {
-    document[navigatePop(494)](navigatePop(476))[navigatePop(481)] = navigatePop(486);
-  } else {
-    document[navigatePop(494)](navigatePop(476))[navigatePop(481)] = navigatePop(488);
-  }
+    const _0xa80748 = _0x4e0e; // some value from the array
+    let _0x3761f8 = document['getElementById'](_0xa80748(0x1e4))[_0xa80748(0x1dd)];
+    for (let _0x16c626 = 0x0; _0x16c626 < _0x3761f8['length']; _0x16c626++) {
+        exports[_0xa80748(0x1d7)](_0x3761f8[_0xa80748(0x1ec)](_0x16c626), _0x16c626);
+    }
+    exports['copy_char'](0x0, _0x3761f8['length']), exports[_0xa80748(0x1e7)]() == 0x1 ? document[_0xa80748(0x1ee)](_0xa80748(0x1dc))[_0xa80748(0x1e1)] = _0xa80748(0x1e6) : document[_0xa80748(0x1ee)](_0xa80748(0x1dc))[_0xa80748(0x1e1)] = _0xa80748(0x1e8);
 }
-;
